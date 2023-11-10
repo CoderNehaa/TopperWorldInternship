50050c2ba9e1a115cc2c30e628b83e5c81d89dd6
Neha <nagdaneha97@gmail.com>
2023-11-10 15:25:38 +0530

    final commit

--- task1LandingPage/script.js
+++ task1LandingPage/script.js
@@ -1,67 +1,15 @@
-// Brand Slogan animation
-
-const text = document.getElementById('brand-slogan').textContent;
-document.getElementById('brand-slogan').innerHTML = ''
-let temp = ''
+// Brand Slogan animation = Reveal letters one by one 
+const text = document.querySelector(".reveal-letters-effect").textContent;
+document.querySelector(".reveal-letters-effect").innerHTML = ''
+let str = ''
 let index = 0;
 
 const animationInterval = setInterval(() => {
     if (index < text.length) {
-        temp = temp + text.charAt(index);
-        document.getElementById('brand-slogan').innerHTML = temp;
+        str = str + text.charAt(index);
+        document.querySelector(".reveal-letters-effect").innerHTML = str;
         index++;
     } else {
         clearInterval(animationInterval); 
     }
 }, 100);
-
-
-// About section heading animation
-
-const aboutHeading = document.querySelector(".aboutHeading");
-const arr = aboutHeading.textContent.split("");
-aboutHeading.textContent = '';
-
-for(let i = 0; i < arr.length; i++){
-    aboutHeading.innerHTML += "<span>" + arr[i] + "</span>"
-}
-
-let char = 0;
-let timer = setInterval(onTick, 50);
-
-function onTick(){
-    const span = aboutHeading.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char++;
-
-    if(char === arr.length){
-        clearInterval(timer);
-        return
-    }
-}
-
-// Reviews section heading animation
-
-const reviewsHeading = document.querySelector(".reviewsHeading");
-const reviewsHeadingArray = reviewsHeading.textContent.split("");
-
-reviewsHeading.textContent = '';
-for(let i = 0; i<reviewsHeadingArray.length; i++){
-    reviewsHeading.innerHTML += "<span>"+reviewsHeadingArray[i]+"</span>"
-}
-
-let reviewHeadingChar = 0;
-let reviewInterval = setInterval(reviewHeadingEffect, 50);
-
-function reviewHeadingEffect(){
-    const span = reviewsHeading.querySelectorAll('span')[reviewHeadingChar];
-    span.classList.add('coloring');
-    reviewHeadingChar++;
-
-    if(reviewHeadingChar === reviewsHeadingArray.length){
-        clearInterval(reviewInterval);
-        reviewInterval = null;
-        return
-    }
-}
-
